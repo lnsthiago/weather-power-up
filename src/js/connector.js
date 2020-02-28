e8d79f13c1b679fee4e18b4bc7656382c058db04
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-02-28 14:58:08 -0300

    Inserindo botão

--- src/js/connector.js
+++ src/js/connector.js
@@ -158,5 +158,17 @@ window.TrelloPowerUp.initialize(
         height: 281,
       });
     },
+    'card-buttons': function (t, options) {
+      return [{
+        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+        text: 'Card Size',
+        callback: function (t) {
+          return t.popup({
+            title: "Card Size",
+            url: 'estimate.html'
+          });
+        }
+      }];
+    },
   }
 );
