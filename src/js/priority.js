dd4f98289b25a93d788f61082d54757338085e45
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-02-29 15:57:25 -0300

    Teste css

--- src/js/priority.js
+++ src/js/priority.js
@@ -2,7 +2,6 @@ var trello = window.TrelloPowerUp.iframe();
 
 window.priority.addEventListener('submit', function (event) {
     event.preventDefault();
-    debugger;
     return trello.set('card', 'shared', 'prioritySize', window.prioritySize.value)
         .then(function () {
             trello.closePopup();
