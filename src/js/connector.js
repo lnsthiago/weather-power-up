62633bd74b42f59e6db84904cbc3f9666301ebce
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-03-02 15:29:11 -0300

    Ajuste icone

--- src/js/connector.js
+++ src/js/connector.js
@@ -11,7 +11,7 @@ const getEstimateBadgesDetails = (t, opts) =>
     const badgeCardEstimate = {
       dynamic(t) {
         return {
-          icon: 'tarefa.svg',
+          icon: './../images/tarefa.svg',
           title: 'SIZE',
           text: cardEstimate || '0',
           color: 'orange'
@@ -22,7 +22,7 @@ const getEstimateBadgesDetails = (t, opts) =>
     const badgeCardSpent = {
       dynamic(t) {
         return {
-          icon: 'tarefa.svg',
+          icon: './tarefa.svg',
           title: 'SPENT',
           text: cardSpent || '0',
           color: 'green'
