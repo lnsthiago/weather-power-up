0428b35551625e83ab0ee62e2bcca6a0a74201cd
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-03-04 13:14:06 -0300

    Testando authorização

--- src/js/connector.js
+++ src/js/connector.js
@@ -133,6 +133,25 @@ function getPriorityColor(prioritySize) {
   return 'grey';
 }
 
+var authenticationSuccess = function() {
+  console.log('Successful authentication');
+};
+
+var authenticationFailure = function() {
+  console.log('Failed authentication');
+};
+
+window.Trello.authorize({
+  type: 'popup',
+  name: 'Getting Started Application',
+  scope: {
+    read: 'true',
+    write: 'true' },
+  expiration: 'never',
+  success: authenticationSuccess,
+  error: authenticationFailure
+});
+
 window.TrelloPowerUp.initialize({
   'card-badges': getEstimateBadgesDetails,
   'card-detail-badges': getEstimateBadgesDetails,
